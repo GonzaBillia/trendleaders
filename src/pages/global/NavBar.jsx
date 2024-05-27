e899d2e7bddd02f919268faec0b0a87a941a2626
Gonzalo Billia <gonzalobillia@gmail.com>
2024-05-27 19:32:13 -0300

    agrego borders active a navlinks

--- src/pages/global/NavBar.jsx
+++ src/pages/global/NavBar.jsx
@@ -5,7 +5,9 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined'
-import { Person2Outlined } from '@mui/icons-material'
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
+import logoDark from '../../assets/logoDark.png'
+import logoLight from '../../assets/logoLight.png'
 
 const NavBar = () => {
     const theme = useTheme()
@@ -18,26 +20,26 @@ const NavBar = () => {
             <Box className='container mx-auto p-5 grid grid-cols-12 justify-between'>
                 <Box className='flex h-8 col-span-3'>
                     <Link to={'/trendleaders/'}>
-                        <img className='h-8' src={theme.palette.mode === 'dark' ? 'trendleaders/src/assets/full-logo-dark.svg' : 'trendleaders/src/assets/full-logo-light.svg'} alt='logo' />
+                        <img className='h-8' src={theme.palette.mode === 'dark' ? {logoDark} : {logoLight}} alt='logo' />
                     </Link>
                 </Box>
                 <Box className='flex justify-around items-center col-span-6'>
-                    <NavLink to={'/trendleaders/services'}>
-                        <Typography variant='h6'>Services</Typography>
-                    </NavLink>
-                    <NavLink to={'/trendleaders/portfolio'}>
+                        <NavLink to={'/trendleaders/services'} className={({ isActive }) => isActive ? 'border-b-4 border-orange-900' : undefined}>
+                            <Typography variant='h6'>Services</Typography>
+                        </NavLink>
+                    <NavLink to={'/trendleaders/portfolio'} className={({ isActive }) => isActive ? 'border-b-4 border-orange-900' : undefined}>
                         <Typography variant='h6'>Portfolio</Typography>
                     </NavLink>
-                    <NavLink to={'/trendleaders/about'}>
+                    <NavLink to={'/trendleaders/about'} className={({ isActive }) => isActive ? 'border-b-4 border-orange-900' : undefined}>
                         <Typography variant='h6'>About Us</Typography>
                     </NavLink>
-                    <NavLink to={'/trendleaders/contact'}>
+                    <NavLink to={'/trendleaders/contact'} className={({ isActive }) => isActive ? 'border-b-4 border-orange-900' : undefined}>
                         <Typography variant='h6'>Contact Us</Typography>
                     </NavLink>
                 </Box>
                 <Box className='col-span-3 flex justify-end'>
                     <IconButton>
-                        <Person2Outlined />
+                        <Person2OutlinedIcon />
                     </IconButton>
                     <IconButton onClick={colorMode.toggleColorMode}>
                         {theme.palette.mode === 'dark' ? (
