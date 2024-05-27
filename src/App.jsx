90d598582d0d135735b3d4e94bca6b45c04a952d
Gonzalo Billia <gonzalobillia@gmail.com>
2024-05-27 16:37:56 -0300

    instalo dependencias de Material UI, configuro ThemeProvider para dark mode, tipografia y CSS basico

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider} from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, colorMode] = useMode()
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ColorModeContext.Provider value={useMode(colorMode)}>
+          <ThemeProvider theme={useMode(theme)[0]}>
+              <CssBaseline />
+              <main className='content'>
+                <NavBar />
+                <Routes>
+                  <Route path="/trendleaders/" element={<Home />} />
+                </Routes>
+                <Footer />
+              </main>
+          </ThemeProvider>
+      </ColorModeContext.Provider>
     </>
   )
 }
