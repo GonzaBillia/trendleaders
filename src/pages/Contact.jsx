af6863ad2aea71409e0ef9e5f6932a6618c962b9
Gonzalo Billia <gonzalobillia@gmail.com>
2024-06-07 13:53:04 -0300

    completo contact us

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Box } from '@mui/material'
+import BannerHeader from '../components/BannerHeader'
+import ContactForm from '../components/ContactForm'
+import PricingOptions from '../components/PricingOptions'
+import ContactQuestions from '../components/ContactQuestions'
 
 const Contact = () => {
     return (
         <Box minHeight={'70vh'}>
-
+            <BannerHeader section={'contact-us'}/>
+            <ContactForm />
+            <PricingOptions />
+            <ContactQuestions />
         </Box>
     )
 }
